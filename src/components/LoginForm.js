27e0e108edbf9ef0f8c6f6f24ac87a9316ec16db
Nicole Eidi <nicoleeidi1@gmail.com>
2018-05-29 21:54:01 +0000

    automatically backed up by learn

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -16,6 +16,7 @@ class LoginForm extends React.Component {
    };
 
    handleFormSubmit = event => {
+     event.preventDefault()
      const username: this.state.username
      const password: this.state.password
 
